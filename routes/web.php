3aefd9faf52264ab18768203527e1337543d304e
Christian01881 <chrispamo01881@gmail.com>
2023-05-23 18:49:25 +0200

    project finished

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/welcome', function () {
     return view('welcome');
-});
+})->name('welcome');
